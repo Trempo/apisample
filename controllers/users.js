9ba79853e23b88ac70ad89458c4437aeb52f0ac0
coldblade2000 <diegogranada7@gmail.com>
2022-03-11 13:25:12 -0500

    implemente hashing de contraseña

--- controllers/users.js
+++ controllers/users.js
@@ -1,45 +1,56 @@
+const { createHash } = require('crypto');
 const mongo = require("../lib/mongo");
+
 const ObjectID = require("mongodb").ObjectId;
-
 function getUsers() {
-  return mongo.then((client) => {
-    return client.db("apisample").collection("users").find({}).toArray();
-  });
-}
+    return mongo.then((client) => {
+        return client.db("apisample").collection("users").find({}).toArray();
+    });
 
+}
 function getUser(id) {
-  return mongo.then((client) => {
-    return client
-      .db("apisample")
-      .collection("users")
-      .findOne({ _id: new ObjectID(id) });
-  });
-}
+    return mongo.then((client) => {
+        return client
+            .db("apisample")
+            .collection("users")
+            .findOne({_id: new ObjectID(id)});
+    });
 
-function createUser(user) {
-  return mongo.then((client) => {
-    return client.db("apisample").collection("users").insertOne(user);
-  });
 }
+function createUser(username, rawPassword, role) {
+    return mongo.then((client) => {
+        return client.db("apisample").collection("users").insertOne(
+            {
+                username,
+                passwordHash: hash(rawPassword),
+                role
+            }
+        );
+    });
 
+}
 function updateUser(id, user) {
-  return mongo.then((client) => {
-    return client
-      .db("apisample")
-      .collection("users")
-      .updateOne({ _id: new ObjectID(id) }, { $set: user });
-  });
-}
+    return mongo.then((client) => {
+        return client
+            .db("apisample")
+            .collection("users")
+            .updateOne({_id: new ObjectID(id)}, {$set: user});
+    });
 
+}
 function deleteUser(id) {
-  return mongo.then((client) => {
-    return client
-      .db("apisample")
-      .collection("users")
-      .deleteOne({ _id: new ObjectID(id) });
-  });
+    return mongo.then((client) => {
+        return client
+            .db("apisample")
+            .collection("users")
+            .deleteOne({_id: new ObjectID(id)});
+    });
+
 }
 
-const user = { getUsers, getUser, createUser, updateUser, deleteUser };
+const user = {getUsers, getUser, createUser, updateUser, deleteUser};
 
+function hash(string) {
+    return createHash('sha256').update(string).digest('hex');
+}
 module.exports = user;
